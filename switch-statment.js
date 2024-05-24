1ad5b41e19ac3ea3a092be5b60f5db8c084e020a
Lucile Jones <jones.lucile@gmail.com>
2024-05-24 12:22:38 -0600

    added switch(true) example

--- switch-statment.js
+++ switch-statment.js
@@ -67,5 +67,65 @@ console.log(dayName);
 // console.log(today);
 
 
+
+
+
 // write a 'switch' statement that assigns a grade based on a score
 // output A, B, C, or D
+// uses switch(true)
+
+// instead of comparing a single value to multiple cases, switch(true) allows each case to be a boolean expression
+// then each case statement contains a condition that evaluates to true or false
+// the switch statement then compares the boolean value 'true' against each case
+// if a case's condition evaluates to true, that block of code is executed
+
+
+function getLetterGrade(score) {
+    let grade;
+
+    switch (true) {
+        case (score >= 90):
+            grade = 'A';
+            break;
+        case (score >= 80):
+            grade = 'B';
+            break;
+        case (score >= 70):
+            grade = 'C';
+            break;
+        case (score >= 60):
+            grade = 'D';
+            break;
+        default:
+            grade = 'F';
+            break;
+    }
+
+    return grade;
+}
+
+console.log(getLetterGrade(95)); // Outputs: A
+console.log(getLetterGrade(82)); // Outputs: B
+console.log(getLetterGrade(75)); // Outputs: C
+console.log(getLetterGrade(63)); // Outputs: D
+console.log(getLetterGrade(50)); // Outputs: F
+
+
+// This could be written as an if/else:
+// function getLetterGrade(score) {
+//     let grade;
+
+//     if (score >= 90) {
+//         grade = 'A';
+//     } else if (score >= 80) {
+//         grade = 'B';
+//     } else if (score >= 70) {
+//         grade = 'C';
+//     } else if (score >= 60) {
+//         grade = 'D';
+//     } else {
+//         grade = 'F';
+//     }
+
+//     return grade;
+// }
